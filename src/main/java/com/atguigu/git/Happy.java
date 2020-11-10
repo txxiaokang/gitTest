abe8ed50ccd83f35a36ca154ef0ef396f0b7c56d
eclipse <532273517@qq.com>
2020-11-10 20:29:39 +0800

    edit on branch hof_fix

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("I'm Bad right !");
 		System.out.println("I'm Happy left !");
+		System.out.println("hot fix...");
 	}
 }
